f116705affcae635fd347c9693980b04bcfcdbc4
otimgren <34794187+otimgren@users.noreply.github.com>
2021-09-01 14:05:44 -0400

    Added setup.py for easy installation

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Python wrapper for PICam'
+LONG_DESCRIPTION = 'Python wrapper for Teledyne Princeton Instruments PICam'
+
+# Setting up
+setup(
+        name="PICamPy", 
+        version=VERSION,
+        author="Oskari Timgren",
+        author_email="<oskari.timgren@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['pythonnet'],    
+        keywords=['python', ],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+        ]
+)
